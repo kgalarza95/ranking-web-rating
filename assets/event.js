3ec57abe3abd4aa0e77d2342f690a93bc9f0f09f
kgalarza <kevin.galarzavera@gmail.com>
2025-03-24 19:38:42 -0500

    edicion de eventos, estilos y home

--- assets/event.js
+++ assets/event.js
@@ -8,18 +8,19 @@ const rankingData = [
     { nombre: "Alain Campoverde", puntos: 0 },
     { nombre: "Paola Barros", puntos: 0 },
     { nombre: "Anthony Loor", puntos: 0 },
-    { nombre: "Austin Salguero", puntos: 0 }
+    { nombre: "Austin Salguero", puntos: 0 },
 ];
 
-const medallas = ["🥇", "🥈", "🥉"];
+const medallas = ["🥇", "🥈", "🥉", "4", "5"];
 
 const rankingList = document.getElementById("ranking-list");
 rankingData.forEach((persona, index) => {
     const div = document.createElement("div");
     div.classList.add("ranking-item");
-    
-    const medal = index < 3 ? `<span class="medal">${medallas[index]}</span>` : "";
 
-    div.innerHTML = `${medal} ${persona.nombre} <span>${persona.puntos} pts</span>`;
+    let identifier = index < 3 ? `<span class="medal">${medallas[index]}</span>` :
+        (index < 5 ? `<span class="position">${index + 1}°</span>` : "");
+
+    div.innerHTML = `${identifier} ${persona.nombre} <span>${persona.puntos} pts</span>`;
     rankingList.appendChild(div);
 });
